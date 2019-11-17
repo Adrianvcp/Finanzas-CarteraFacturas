fa00b96495383468b90356acb14f42ead7a2b91f
Adrian Vela <u201416198@upc.edu.pe>
2019-11-17 17:46:38 -0500

    version 1 - login falta redireccion, regresa los bancos

---
+++ TrabajoFinanzas/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoFinanzas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult CerrarSesion()
+        {
+
+            Session["estado"] = 0;
+            Session["userName"] = "";
+            Session["idUser"] = 0;
+            Session["plazoGracia"] = 0;
+            Session["frecuencia"] = 0;
+            Session["frDias"] = 0;
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
